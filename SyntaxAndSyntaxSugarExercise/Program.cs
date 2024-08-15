8cc1c123ae193c938e572d4b02738b5b4ce6859c
Filip Herbst <filip_herbst@yahoo.com>
2024-08-14 23:40:06 -0500

    Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer1 = 4;
+            var answer2 = 9;
+            //var response = "";
+            
+            //First Version w/ If Else (Commented Out Below):
+            //_______________________________________________
+            
+            //if (answer1 < answer2)
+            //{
+                //response = answer1 + $" is less than {answer2}.";
+            //}
+            //else
+            //{
+                //response = answer1 + $" is greater than or equal to {answer2}.";
+            //}
+            
+            //Console.WriteLine($"{response}");
+            
+            
+            
+            //Second Version w/ Ternary (Below):
+            //_________________________________
+            
+            //Condition, TrueScope, FalseScope
+            var response = (answer1 < answer2)
+                ? answer1 + $" is less than {answer2}."
+                : answer1 + $" is greater than or equal to {answer2}.";
+            
+            Console.WriteLine(response);
+
         }
     }
 }
